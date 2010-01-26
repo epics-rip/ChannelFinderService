dce99a8f3c6d45d93532e6f242df83269964a298
rlange <rlange@nsls2rlange>
2010-01-26 18:10:09 -0500

    Added JAXBContextResolver to allow finetuning of JSON output

---
+++ src/java/gov/bnl/channelfinder/MyJAXBContextResolver.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gov.bnl.channelfinder;
+
+import com.sun.jersey.api.json.JSONJAXBContext;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.ext.ContextResolver;
+import javax.xml.bind.JAXBContext;
+
+/**
+ *
+ * @author rlange
+ */
+@Provider
+public class MyJAXBContextResolver implements ContextResolver<JAXBContext> {
+
+    private JAXBContext context;
+    private Class[] types = {XmlChannels.class, XmlProperties.class};
+
+    public MyJAXBContextResolver() throws Exception {
+        Map props = new HashMap<String, Object>();
+        props.put(JSONJAXBContext.JSON_ROOT_UNWRAPPING, Boolean.FALSE);
+        this.context = new JSONJAXBContext(types, props);
+    }
+
+    public JAXBContext getContext(Class<?> objectType) {
+        return (types[0].equals(objectType)) ? context : null;
+    }
+}
