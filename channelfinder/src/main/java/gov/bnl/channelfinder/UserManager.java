b088d4c10819aa6d0106da75e36bf20cbc7d6dde
Ralph Lange <Ralph.Lange@bessy.de>
2010-05-06 10:33:56 -0400

    UserManager: Made constructor private, added Javadoc.

--- channelfinder/src/main/java/gov/bnl/channelfinder/UserManager.java
+++ channelfinder/src/main/java/gov/bnl/channelfinder/UserManager.java
@@ -28,21 +28,42 @@ public class UserManager {
         groups.put("channy", "group3");
     }
 
-    public UserManager() {
+    private UserManager() {
     }
 
+    /**
+     * Returns the (singleton) instance of UserManager.
+     *
+     * @return instance of UserManager
+     */
     public static UserManager getInstance() {
         return instance;
     }
 
+    /**
+     * Sets the (thread local) user principal to be used in further calls.
+     *
+     * @param user principal
+     */
     public void setUser(Principal user) {
         this.user.set(user);
     }
 
+    /**
+     * Checks if the user is in the specified <tt>group</tt>.
+     *
+     * @param group name of the group to check membership
+     * @return true if user is a member of <tt>group</tt>
+     */
     public boolean isUserInGroup(String group) {
         return groups.containsEntry(getUserName(), group);
     }
 
+    /**
+     * Returns the current user's name.
+     *
+     * @return name of current user
+     */
     public String getUserName() {
         return user.get().getName();
     }
