b3fc646e8384a03011a5fae7ee0327e53caa3293
Ralph Lange <Ralph.Lange@bessy.de>
2010-05-26 17:33:26 -0400

    UserManager: Added JNDI based LDAP connection to query for group ownerships.

--- channelfinder/src/main/java/gov/bnl/channelfinder/UserManager.java
+++ channelfinder/src/main/java/gov/bnl/channelfinder/UserManager.java
@@ -5,9 +5,18 @@
 
 package gov.bnl.channelfinder;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
 import java.security.Principal;
+import java.util.Collection;
+import java.util.HashSet;
+import javax.annotation.Resource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
 
 /**
  * Group Manager: Manages user -> group map
@@ -16,17 +25,12 @@ import java.security.Principal;
 public class UserManager {
     private static UserManager instance = new UserManager();
     private ThreadLocal<Principal> user = new ThreadLocal<Principal>();
-    private static Multimap<String, String> groups = ArrayListMultimap.create();
+    private ThreadLocal<Collection<String>> groups = new ThreadLocal<Collection<String>>();
+    private ThreadLocal<DirContext> ctx = new ThreadLocal<DirContext>();
+    private static final String ldapResourceName = "channelfinderGroups";
 
-    static {
-        /* FIXME: For the time being: hardcoded */
-        groups.put("taggy", "group1");
-        groups.put("proppy", "group1");
-        groups.put("proppy", "group2");
-        groups.put("channy", "group1");
-        groups.put("channy", "group2");
-        groups.put("channy", "group3");
-    }
+    @Resource(name="ldapGroupMemberField") protected String memberUidField = "memberUid";
+    @Resource(name="ldapGroupTargetField") protected String groupTargetField = "cn";
 
     private UserManager() {
     }
@@ -40,13 +44,57 @@ public class UserManager {
         return instance;
     }
 
+    private void clearGroups() {
+        if (this.groups.get() == null) {
+            this.groups.set(new HashSet<String>());
+        } else {
+            this.groups.get().clear();
+        }
+    }
+
+    private DirContext getJndiContext() {
+        DirContext dirctx = this.ctx.get();
+        if (dirctx == null) {
+            try {
+                Context initCtx = new InitialContext();
+                dirctx = (DirContext) initCtx.lookup(ldapResourceName);
+                this.ctx.set(dirctx);
+            } catch (NamingException e ) {
+                throw new IllegalStateException("Cannot find JNDI LDAP resource '"
+                        + ldapResourceName + "'", e);
+            }
+        }
+        return dirctx;
+    }
+
     /**
-     * Sets the (thread local) user principal to be used in further calls.
+     * Sets the (thread local) user principal to be used in further calls, initializes
+     * the LDAP directory context, if necessary, then queries LDAP to find the groups
+     * mapping for the given user.
      *
      * @param user principal
      */
     public void setUser(Principal user) {
         this.user.set(user);
+        clearGroups();
+        DirContext dirctx = getJndiContext();
+        try {
+            SearchControls ctrls = new SearchControls();
+            ctrls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+
+            String searchfilter = "(" + memberUidField + "=" + this.user.get().getName() + ")";
+            NamingEnumeration<SearchResult> result = dirctx.search("", searchfilter, ctrls);
+
+            while (result.hasMore()) {
+                Attribute att = result.next().getAttributes().get(groupTargetField);
+                if (att != null) {
+                    groups.get().add((String)att.get());
+                }
+            }
+        } catch (Exception e) {
+                throw new IllegalStateException("Error while retrieving group information for user '"
+                        + this.user.get().getName() + "'", e);
+        }
     }
 
     /**
@@ -55,8 +103,8 @@ public class UserManager {
      * @param group name of the group to check membership
      * @return true if user is a member of <tt>group</tt>
      */
-    public boolean isUserInGroup(String group) {
-        return groups.containsEntry(getUserName(), group);
+    public boolean userIsInGroup(String group) {
+        return groups.get().contains(group);
     }
 
     /**
