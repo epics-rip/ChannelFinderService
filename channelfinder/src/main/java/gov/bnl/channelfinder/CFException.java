2bcd502057bb784f66898b6d726e8629463a2ea5
Ralph Lange <Ralph.Lange@bessy.de>
2010-06-11 15:17:27 -0400

    Changed error message payload to avoid html decorations.

--- channelfinder/src/main/java/gov/bnl/channelfinder/CFException.java
+++ channelfinder/src/main/java/gov/bnl/channelfinder/CFException.java
@@ -45,7 +45,7 @@ public class CFException extends Exception {
         String msg = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"" +
                 " \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">" +
                 "<html><head><title>ChannelServer - Error report</title></head>" +
-                "<body><h1>HTTP Status " + this.status.getStatusCode() + " - " + this.status + "</h1><hr/>" +
+                "<body><h1>HTTP Status " + this.status.getStatusCode() + " - " + this.status.getReasonPhrase() + "</h1><hr/>" +
                 "<p><b>message</b></p><p>" + getMessage() + "</p>";
         if (this.getCause() != null) {
             msg = msg + "<p><b>caused by:</b></p><p>" + this.getCause().getMessage() + "</p>";
