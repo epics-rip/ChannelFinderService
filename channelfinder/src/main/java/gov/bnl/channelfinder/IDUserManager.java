7a390439978a06814d94a74c0e2c7ed0b04e5950
Gabriele Carcassi <carcassi@bnl.gov>
2011-03-25 11:50:52 -0400

    Adding switch to decide how to resolve to groups. Implementation using 'id' command. Adding glassfish-web.xml for glassfish 3.1.

---
+++ channelfinder/src/main/java/gov/bnl/channelfinder/IDUserManager.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2010 Brookhaven National Laboratory
+ * Copyright (c) 2010 Helmholtz-Zentrum Berlin für Materialien und Energie GmbH
+ * Subject to license terms and conditions.
+ */
+package gov.bnl.channelfinder;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Uses Unix command 'id' to determine the group membership.
+ *
+ * @author Ralph Lange <Ralph.Lange@bessy.de>
+ */
+public class IDUserManager extends UserManager {
+
+    public static String readInputStreamAsString(InputStream in) throws IOException {
+        BufferedInputStream bis = new BufferedInputStream(in);
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        int result = bis.read();
+        while (result != -1) {
+            byte b = (byte) result;
+            buf.write(b);
+            result = bis.read();
+        }
+        return buf.toString();
+    }
+
+    @Override
+    protected Set<String> getGroups(Principal user) {
+        try {
+            Set<String> groups = new HashSet<String>();
+            ProcessBuilder pb = new ProcessBuilder("id", user.getName());
+            Process proc = pb.start();
+            String output = readInputStreamAsString(proc.getInputStream());
+            output = output.substring(output.indexOf("groups"));
+            
+            Pattern pattern = Pattern.compile("\\((.*?)\\)");
+            Matcher match = pattern.matcher(output);
+            while (match.find()) {
+                groups.add(match.group(1));
+            }
+            return groups;
+        } catch (Exception e) {
+            throw new IllegalStateException("Error while retrieving group information for user '"
+                    + user.getName() + "'", e);
+        }
+    }
+}
