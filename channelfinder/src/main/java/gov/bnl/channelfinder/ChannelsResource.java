807c1b777d0d7808421405181ce9ac42205ab849
Kunal Shroff <shroffk@bnl.gov>
2015-06-22 11:39:46 -0400

    Implementing a rudimentary search for the channelfinder index

--- channelfinder/src/main/java/gov/bnl/channelfinder/ChannelsResource.java
+++ channelfinder/src/main/java/gov/bnl/channelfinder/ChannelsResource.java
@@ -10,10 +10,17 @@ package gov.bnl.channelfinder;
  * #L%
  */
 
-import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.disMaxQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.elasticsearch.index.query.QueryBuilders.nestedQuery;
+import static org.elasticsearch.index.query.QueryBuilders.wildcardQuery;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map.Entry;
 import java.util.logging.Logger;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -23,20 +30,26 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.DisMaxQueryBuilder;
+import org.elasticsearch.search.SearchHit;
 /**
  * Top level Jersey HTTP methods for the .../channels URL
  * 
@@ -68,24 +81,120 @@ public class ChannelsResource {
     @GET
     @Produces({"application/xml", "application/json"})
     public Response query() {
-        DbConnection db = DbConnection.getInstance();
-        ChannelManager cm = ChannelManager.getInstance();
+        Client client = new TransportClient().addTransportAddress(new InetSocketTransportAddress("130.199.219.147", 9300));
+/**
+ * Current Mapping for the channel 
+ * PUT /channelfinder/_mapping/channel
+{
+  "channel": {
+    "properties": {
+      "name": {
+        "type": "string",
+        "analyzer": "whitespace"
+      },
+      "owner": {
+        "type": "string",
+        "analyzer": "whitespace"
+      },
+      "xmlProperties": {
+        "properties": {
+          "properties": {
+            "type": "nested",
+            "include_in_parent": true,
+            "properties": {
+              "name": {
+                "type": "string",
+                "analyzer": "whitespace"
+              },
+              "owner": {
+                "type": "string"
+              },
+              "value": {
+                "type": "string",
+                "analyzer": "whitespace"
+              }
+            }
+          }
+        }
+      },
+      "xmlTags": {
+        "properties": {
+          "tags": {
+            "type": "nested",
+            "properties": {
+              "name": {
+                "type": "string",
+                "analyzer": "whitespace"
+              },
+              "owner": {
+                "type": "string",
+                "analyzer": "whitespace"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+ */
         String user = securityContext.getUserPrincipal() != null ? securityContext.getUserPrincipal().getName() : "";
         try {
-            db.getConnection();
-            db.beginTransaction();
-            XmlChannels result = cm.findChannelsByMultiMatch(uriInfo.getQueryParameters());
-            db.commit();
+            MultivaluedMap<String, String> parameters = uriInfo.getQueryParameters();
+            BoolQueryBuilder qb = boolQuery();
+            DisMaxQueryBuilder nameQuery = disMaxQuery();
+            
+            
+            for (Entry<String, List<String>> parameter : parameters.entrySet()) {
+                switch (parameter.getKey()) {
+                case "~name":
+                    for (String pattern : parameter.getValue()) {
+                        nameQuery.add(wildcardQuery("name", pattern));
+                    }
+                    qb.must(nameQuery);
+                    break;
+                case "~tag":
+                    DisMaxQueryBuilder tagQuery = disMaxQuery();
+                    for (String pattern : parameter.getValue()) {
+                        tagQuery.add(wildcardQuery("xmlTags.tags.name", pattern));
+                    }
+                    qb.must(nestedQuery("xmlTags.tags", tagQuery));
+                    break;
+                default:
+                    DisMaxQueryBuilder propertyQuery = disMaxQuery();
+                    for (String pattern : parameter.getValue()) {
+                        propertyQuery
+                                .add(nestedQuery("xmlProperties.properties",
+                                        boolQuery()
+                                                .must(matchQuery("xmlProperties.properties.name", parameter.getKey()))
+                                                .must(wildcardQuery("xmlProperties.properties.value", pattern))));
+                    }
+                    qb.must(propertyQuery);
+                    break;
+                }
+            }
+            
+            long start = System.currentTimeMillis();
+            SearchResponse qbResult = client.prepareSearch("channelfinder").setQuery(qb).setSize(10000).execute().actionGet();
+            audit.info("Query: " + (System.currentTimeMillis() - start));
+            audit.info("Query Size : " + qbResult.getHits().getTotalHits());
+            ObjectMapper mapper = new ObjectMapper();
+            XmlChannels result = new XmlChannels();
+            start = System.currentTimeMillis();
+            if(qbResult != null){
+                for (SearchHit hit : qbResult.getHits()) {
+                    result.addXmlChannel(mapper.readValue(hit.source(), XmlChannel.class));
+                }
+            }
+            audit.info("Parse: " + (System.currentTimeMillis() - start));
             Response r = Response.ok(result).build();
             log.fine(user + "|" + uriInfo.getPath() + "|GET|OK|" + r.getStatus()
                     + "|returns " + result.getChannels().size() + " channels");
             return r;
-        } catch (CFException e) {
-            log.warning(user + "|" + uriInfo.getPath() + "|GET|ERROR|"
-                    + e.getResponseStatusCode() +  "|cause=" + e);
-            return e.toResponse();
+        } catch (Exception e) {
+            return handleException(user, "GET", Response.Status.INTERNAL_SERVER_ERROR, e);
         } finally {
-            db.releaseConnection();
+            client.close();
         }
     }
 
@@ -99,30 +208,35 @@ public class ChannelsResource {
     @POST
     @Consumes({"application/xml", "application/json"})
     public Response add(XmlChannels data) throws IOException {
-        DbConnection db = DbConnection.getInstance();
-        ChannelManager cm = ChannelManager.getInstance();
+        Client client = new TransportClient().addTransportAddress(new InetSocketTransportAddress("130.199.219.147", 9300));
         UserManager um = UserManager.getInstance();
         um.setUser(securityContext.getUserPrincipal(), securityContext.isUserInRole("Administrator"));
+        ObjectMapper mapper = new ObjectMapper();
         try {
-            cm.checkValidNameAndOwner(data, chNameRegex);
-            cm.checkValidValue(data);
-            db.getConnection();
-            db.beginTransaction();
-            if (!um.userHasAdminRole()) {
-                cm.checkUserBelongsToGroup(um.getUserName(), data);
+            long start = System.currentTimeMillis();
+            BulkRequestBuilder bulkRequest = client.prepareBulk();
+            for (XmlChannel channel : data.getChannels()) {
+                bulkRequest.add(client.prepareUpdate("channelfinder", "channel", channel.getName()).setDoc(mapper.writeValueAsBytes(channel))
+                        .setUpsert(new IndexRequest("channelfinder", "channel", channel.getName()).source(mapper.writeValueAsBytes(channel))));
             }
-            cm.createOrReplaceChannels(data);
-            db.commit();
-            Response r = Response.noContent().build();
-            audit.info(um.getUserName() + "|" + uriInfo.getPath() + "|POST|OK|" + r.getStatus()
-                    + "|data=" + XmlChannels.toLog(data));
-            return r;
-        } catch (CFException e) {
-            log.warning(um.getUserName() + "|" + uriInfo.getPath() + "|POST|ERROR|" + e.getResponseStatusCode()
-                    + "|data=" + XmlChannels.toLog(data) + "|cause=" + e);
-            return e.toResponse();
+            String prepare = "|Prepare: " + (System.currentTimeMillis()-start) + "|";
+            start = System.currentTimeMillis();
+            BulkResponse bulkResponse = bulkRequest.execute().actionGet();
+            String execute = "|Execute: " + (System.currentTimeMillis()-start)+"|";
+            start = System.currentTimeMillis();
+            if (bulkResponse.hasFailures()) {
+                audit.severe(bulkResponse.buildFailureMessage());
+                throw new Exception();
+            } else {
+                Response r = Response.noContent().build();
+                audit.info(um.getUserName() + "|" + uriInfo.getPath() + "|POST|OK|" + r.getStatus() + prepare + execute + "|data="
+                        + XmlChannels.toLog(data));
+                return r;
+            }
+        } catch (Exception e) {
+            return handleException(um.getUserName(), "POST", Response.Status.INTERNAL_SERVER_ERROR, e);
         } finally {
-            db.releaseConnection();
+            client.close();
         }
     }
 
@@ -143,7 +257,7 @@ public class ChannelsResource {
         String user = securityContext.getUserPrincipal() != null ? securityContext.getUserPrincipal().getName() : "";
         XmlChannel result = null;
         try {
-            GetResponse response = client.prepareGet("channels", "channel", chan).execute().actionGet();
+            GetResponse response = client.prepareGet("channelfinder", "channel", chan).execute().actionGet();
             ObjectMapper mapper = new ObjectMapper();
             result = mapper.readValue(response.getSourceAsBytes(), XmlChannel.class);
             Response r;
@@ -181,9 +295,9 @@ public class ChannelsResource {
         um.setUser(securityContext.getUserPrincipal(), securityContext.isUserInRole("Administrator"));
         ObjectMapper mapper = new ObjectMapper();
         try {
-            IndexRequest indexRequest = new IndexRequest("channels", "channel", chan)
+            IndexRequest indexRequest = new IndexRequest("channelfinder", "channel", chan)
                     .source(mapper.writeValueAsBytes(data));
-            UpdateRequest updateRequest = new UpdateRequest("channels", "channel", chan)
+            UpdateRequest updateRequest = new UpdateRequest("channelfinder", "channel", chan)
                     .doc(mapper.writeValueAsBytes(data)).upsert(indexRequest);
             UpdateResponse result = client.update(updateRequest).get();
             Response r = Response.noContent().build();
@@ -216,7 +330,7 @@ public class ChannelsResource {
         um.setUser(securityContext.getUserPrincipal(), securityContext.isUserInRole("Administrator"));
         ObjectMapper mapper = new ObjectMapper();
         try {
-            UpdateRequest updateRequest = new UpdateRequest("channels", "channel", chan)
+            UpdateRequest updateRequest = new UpdateRequest("channelfinder", "channel", chan)
                     .doc(mapper.writeValueAsBytes(data));
             UpdateResponse result = client.update(updateRequest).get();
             Response r = Response.noContent().build();
@@ -245,7 +359,7 @@ public class ChannelsResource {
         UserManager um = UserManager.getInstance();
         um.setUser(securityContext.getUserPrincipal(), securityContext.isUserInRole("Administrator"));
         try {
-            DeleteResponse response = client.prepareDelete("channels", "channel", chan).execute().get();
+            DeleteResponse response = client.prepareDelete("channelfinder", "channel", chan).execute().get();
             Response r = Response.ok().build();
             audit.info(um.getUserName() + "|" + uriInfo.getPath() + "|DELETE|OK|" + r.getStatus());
             return r;
