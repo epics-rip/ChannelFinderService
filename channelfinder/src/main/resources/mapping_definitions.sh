f77c70d2f0af1e0339d1f983f4db481f4f6b6af1
Jeong Han Lee <jeonghan.lee@gmail.com>
2020-02-06 18:15:19 -0800

    Replace string to text for the full searchable. From ES55, the type of string was removed. In order to sync with REAME, ES6 should be used.

--- channelfinder/src/main/resources/mapping_definitions.sh
+++ channelfinder/src/main/resources/mapping_definitions.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ###
 # #%L
 # ChannelFinder Directory Service
+# %%
+# Copyright (C) 2020        Lawrence Berkeley National Laboratory
 # %%
 # Copyright (C) 2010 - 2016 Helmholtz-Zentrum Berlin für Materialien und Energie GmbH
 # %%
@@ -13,68 +15,81 @@
 
 # The mapping definition for the Indexes associated with the channelfinder v2
 
+function print_help()
+{
+    local type=$1; shift;
+    local index=$1; shift;
+    printf "\n>>> %s : %s ....\n" "$type" "$index";
+}
 
 #Create the Index
+print_help "index" "tags"
 curl -XPUT 'http://localhost:9200/tags'
+
 #Set the mapping
-curl -XPUT 'http://localhost:9200/tags/_mapping/tag' -d'
+print_help "mapping" "tags"
+curl -H 'Content-Type: application/json' -XPUT 'http://localhost:9200/tags/_mapping/tag' -d'
 {
   "tag" : {
     "properties" : {
       "name" : {
-        "type" : "string"
+        "type" : "text"
       },
       "owner" : {
-        "type" : "string"
+        "type" : "text"
       }
     }
   }
 }'
 
+print_help "index" "properties"
 curl -XPUT 'http://localhost:9200/properties'
-curl -XPUT 'http://localhost:9200/properties/_mapping/property' -d'
+print_help "mapping" "properties"
+curl -H 'Content-Type: application/json' -XPUT 'http://localhost:9200/properties/_mapping/property' -d'
 {
   "property" : {
     "properties" : {
       "name" : {
-        "type" : "string"
+        "type" : "text"
       },
       "owner" : {
-        "type" : "string"
+        "type" : "text"
       }
     }
   }
 }'
 
+print_help "index" "channelfinder"
 curl -XPUT 'http://localhost:9200/channelfinder'
-curl -XPUT 'http://localhost:9200/channelfinder/_mapping/channel' -d'
+print_help "mapping" "channelfinder"
+curl -H 'Content-Type: application/json' -XPUT 'http://localhost:9200/channelfinder/_mapping/channel' -d'
 {
   "channel" : {
     "properties" : {
       "name" : {
-        "type" : "string",
+        "type" : "text",
         "analyzer" : "whitespace"
       },
       "owner" : {
-        "type" : "string",
+        "type" : "text",
         "analyzer" : "whitespace"
       },
       "script" : {
-        "type" : "string"
+        "type" : "text"
       },
       "properties" : {
         "type" : "nested",
         "include_in_parent" : true,
         "properties" : {
           "name" : {
-            "type" : "string",
+            "type" : "text",
             "analyzer" : "whitespace"
           },
           "owner" : {
-            "type" : "string"
+            "type" : "text"
           },
           "value" : {
-            "type" : "string",
+            "type" : "text",
             "analyzer" : "whitespace"
           }
         }
@@ -84,11 +99,11 @@ curl -XPUT 'http://localhost:9200/channelfinder/_mapping/channel' -d'
         "include_in_parent" : true,
         "properties" : {
           "name" : {
-            "type" : "string",
+            "type" : "text",
             "analyzer" : "whitespace"
           },
           "owner" : {
-            "type" : "string",
+            "type" : "text",
             "analyzer" : "whitespace"
           }
         }
@@ -96,3 +111,5 @@ curl -XPUT 'http://localhost:9200/channelfinder/_mapping/channel' -d'
     }
   }
 }'
+
+printf "\n";
